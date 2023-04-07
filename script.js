1675edb2ab83269cb023412fb817c6d9bf8f8944
Abstrakcionist <kondratev04g@mail.ru>
2023-04-07 19:32:19 +0500

    конец игры

--- script.js
+++ script.js
@@ -164,3 +164,21 @@ function placeTetromino(){
     }
     tetromino = getNextTetromino();
 }
+
+function showGameOver() {
+    //прекращаем всю анимацию игры
+    cancelAnimationFrame(rAF);
+    //флаг окончания
+    gameOver = true;
+    //чёрный прямоугольник посередине поля
+    context.fillStyle = 'black';
+    context.globalAlpha = 0.75;
+    context.fillRect(0, canvas.height / 2 - 30, canvas.width, 60);
+    //надпись белым моноширинным шрифтом по центру
+    context.globalAlpha = 1;
+    context.fillStyle = 'white';
+    context.font = '36px monospace';
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    context.fillText('GAME OVER!', canvas.width / 2, canvas.height / 2);
+}
