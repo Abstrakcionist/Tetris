9ce7198fb2e0adbfb47d4094bc2c284bb06ffba2
Abstrakcionist <kondratev04g@mail.ru>
2023-04-07 18:19:15 +0500

    случайная последовательность

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const context = canvas.getContext('2d');
 const grid = 32;
 //массив с последовательностью фигур, в начале пустой
 let tetrominoSequence = [];
-// размер поля — 10 на 20, и несколько строк ещё находится за видимой областью
+//размер поля — 10 на 20 + несколько строк ещё находится за видимой областью
 let playfield = [];
 for (let row = -2; row < 20; row++) {
     playfield[row] = [];
@@ -15,7 +15,7 @@ for (let row = -2; row < 20; row++) {
     }
 }
 
-// задаём формы для каждой фигуры
+//формы для каждой фигуры
 const tetrominos = {
     'I': [
         [0,0,0,0],
@@ -54,7 +54,7 @@ const tetrominos = {
     ]
 }
 
-// цвет каждой фигуры
+//цвет каждой фигуры
 const colors = {
     'I': 'cyan',
     'O': 'yellow',
@@ -64,3 +64,29 @@ const colors = {
     'J': 'blue',
     'L': 'orange'
 };
+
+//счётчик
+let count = 0;
+//текущая фигура в игре
+let tetromino = getNextTetromino();
+//следим за кадрами анимации, чтобы если что — остановить игру
+let rAF = null;
+//флаг конца игры, на старте — неактивный
+let gameOver = false;
+//возвращает случайное число в заданном диапазоне
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//последовательность фигур, которая появится в игре
+function generateSequence() {
+    const sequence = ['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
+    //получение случайной фигуры
+    while (sequence.length){
+        const rand = getRandomInt(0, sequence.length - 1);
+        const name = sequence.splice(rand, 1)[0];
+        //добавление фигуры в массив игры
+        tetrominoSequence.push(name);
+    }
+}
